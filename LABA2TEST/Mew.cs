384a98fbe1c81fd4d3d1dd701086dd1ca0f22aab
Давид Дарбинян <school152qq@mail.ru>
2024-02-09 02:42:12 +0700

    Добавьте файлы проекта.

---
+++ LABA2TEST/Mew.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+
+namespace LABA2TEST
+{
+    public class A{
+        public int firstproperty { get; set; }
+        public int secondproperty { get; set; }
+        public A(int fs,int ss)
+        {
+            this.firstproperty = fs; this.secondproperty = ss;
+        }
+    }
+    public class B
+    {
+        public int firstproperty { get; set; }
+        public string secondproperty { get; set; }
+        public B(int fs,string ss)
+        {
+            firstproperty = fs;
+            secondproperty = ss;
+        }
+    }
+    public class C
+    {
+        public int firstproperty { get; set; }
+        public string secondproperty { get; set; }
+        public C(int firstproperty, string secondproperty)
+        {
+            this.firstproperty = firstproperty;
+            this.secondproperty = secondproperty;
+        }
+    }
+    public class D
+    {
+        public int firstproperty { get; set; }
+        public int secondproperty { get; set; }
+        public D(int firstproperty, int secondproperty)
+        {
+            this.firstproperty = firstproperty;
+            this.secondproperty = secondproperty;
+        }
+    }
+}
---
+++ LABA2TEST/Program.cs
@@ -0,0 +1,91 @@
+﻿using LABA2TEST;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+namespace Laba2Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> list1 = new List<object>()
+            {
+                new A(1,1),
+                new B(1,"1"),
+                new C(1,"aboba"),
+                new B(1,"Hello")
+            };
+            List<object> list2 = new List<object>()
+            {
+                new A(1,1),
+                new B(1,"1"),
+                new C(1,"abob"),
+                new B(1,"Hell")
+ 
+            };
+            bool areEqual = CompareLists(list1, list2);
+
+            // Выводим результат сравнения
+            Console.WriteLine($"Равны ли коллекции? {areEqual}");
+        }
+
+        static bool CompareLists(List<object> list1, List<object> list2)
+        {
+            // Проверяем, что списки имеют одинаковую длину
+
+            if (list1.Count != list2.Count)
+                return false;
+
+            // Проходимся по каждому объекту в списках
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                // Получаем типы объекта
+
+                Type type = list1[i].GetType();
+                Type type1 = list2[i].GetType();
+                
+                // Сравниваем типы объекта
+
+                if (!(type == type1))
+                {
+                    Console.WriteLine($"Найдено расхождение в типах!\nВ позиции: {i+1}\nПолучено: {type1.Name}" +
+                            $"\nОжидалось: {type.Name}\n");
+                    if (i != list1.Count-1)
+                    {
+                        continue; 
+                    }
+                    return false;
+                }
+                // Получаем все свойства объекта
+
+                PropertyInfo[] properties = type.GetProperties();
+                
+                // Проходимся по каждому свойству объекта
+
+                foreach (PropertyInfo property in properties)
+                {
+                    // Получаем значение свойства для каждого списка
+
+                    object ?value1 = property.GetValue(list1[i]);
+                    object ?value2 = property.GetValue(list2[i]);
+
+                    // Сравниваем значения свойств
+
+                    if (!value1.Equals(value2))
+                    {
+                        Console.WriteLine($"Найдено расхождение в значениях!\nВ позиции: {i+1}\nПолучено: {property.GetValue(list2[i])}" +
+                            $"\nОжидалось: {property.GetValue(list1[i])}\n");
+                        if (i != list1.Count - 1)
+                        {
+                            continue;
+                        }
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+    
+}
